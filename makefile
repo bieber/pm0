3d0b39bd5dd18191d56625f4ec94075df87d6a6b
Robert Bieber <robby@bieberphoto.com>
2010-03-11 02:50:31 -0500

    Gutted the parser to make it compile and completed the input functionality, modified the makefile to compile it

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-all: vm scanner symtable.o
+all: vm scanner parser
 
 vm: codes.h vm.c
 	gcc -g -Wall -Wformat -Werror -o vm vm.c
@@ -8,6 +8,9 @@ scanner: scanner.c dfa.o
 
 dfa.o: dfa.c dfa.h codes.h
 	gcc -g -Wall -Wformat -Werror -c -o dfa.o dfa.c
+
+parser: symtable.o parser.c
+	gcc -g -Wall -Wformat -Werror -o parser parser.c symtable.o
 
 symtable.o: symtable.h symtable.c
 	gcc -g -Wall -Wformat -Werror -c -o symtable.o symtable.c
--- parser.c
+++ parser.c
@@ -1,291 +1,140 @@
-#include "codes.h"
-#include "symtable.c"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_SYMBOL_TABLE_SIZE 
+#include "codes.h"
+#include "symtable.h"
 
-#define USAGE "Usage: parser [-q] infile\n"
+#define USAGE "Usage: parser [-q] infile"
 
-void program(struct symbol* symbolList);
-void block(struct symbol* symbolList);
-void statement(struct symbol* symbolList);
-void condition(struct symbol* symbolList);
-void expression(struct symbol* symbolList);
-void term(struct symbol* symbolList);
-void factor(struct symbol* symbolList);
+/*===============*/
+//---Globals-----//
 
-int main(int argc, char* argv[])
-{
-  FILE* fin;
-  int verbose = 1;
-  int fileArg = 1;
-  struct tokenList* tokenTable
-  struct symbol* symbolList;
-  struct symbol* tempList;
-  int L = 0, M = 0;
-  
+// I/O vars
+FILE* fin;
+int verbose = 1;
+
+//Parsing status vars
+token currentToken;
+union{
+  int numeric;
+  char string[MAX_IDENT_LENGTH + 1];
+} tokenVal;
+
+//The symbol table
+symTableList** symTable;
+
+//----------------//
+/*================*/
+
+//Input function
+void readToken();
+
+//Non-terminal parsing functions
+void program();
+void block();
+void statement();
+void condition();
+void expression();
+void term();
+void factor();
+
+int main(int argc, char* argv[]){
+
   //Checking command-line arguments
-  if(argc < 2 || argc > 3){
+  if(argc > 3){
     printf("%s\n", USAGE);
     return 1;
-  }
-  
-  if(argc == 0){
-    fin = STDIN;
-  }
-  else if(argc == 1){
-    if(strcmp(argv[1], "-q") == 0)
+  }else if(argc < 2){
+    fin = stdin;
+  }else if(argc == 2){
+    if(strcmp(argv[1], "-q") == 0){
       verbose = 0;
-    fin = STDIN;
-  }
-  else if(argc == 2){
-    fileArg = 1;
-    fin = fopen(argv[fileArg], "r");
-    tokenTable = system("./scanner -q fin");
-  }
-  //Turning off the verbose flag, if necessary
-  else if(argc == 3){
-    fileArg = 2;
-    if(strcmp(argv[1], "-q") == 0)
-      verbose = 0;
-    
-    fin = fopen(argv[fileArg], "r");
-    tokenTable = system("./scanner -q fin");
-  }
-  
-  tempList = symbolList;
-  
-  while(tokenTable)
-  {
-    tempList.kind = tokenTable.symbol;
-    strcpy(tempList.name, tokenTable.lexeme);
-    
-    if(tempList.kind == numbersym)
-      tempList.val = atoi(tokenTable.lexeme);
-      
-    if(tempList.kind == identsym){
-      tempList.level = ;
-      tempList.addr = ;
+      fin = stdin;
+    }else{
+      fin = fopen(argv[1], "r");
+      if(!fin){
+        printf("%s\n", USAGE);
+        return 1;
+      }
     }
-    
-    if(tempList.kind == procsym){
-      tempList.level = ;
-      tempList.addr = ;
+  }else if(argc == 2){
+    if(strcmp("-q", argv[1]) != 0){
+      printf("%s\n", USAGE);
+      return 1;
     }
-    
-    tokenTable = tokenTable.next;
-    tempList = tempList.next;
+    verbose = 0;
+    fin = fopen(argv[2], "r");
   }
+
+  //Initializing the symbol table
+  symTable = newTable();
+
+  //Fetching the first token
+  readToken();
+  
+  //Parsing the program
+  program();
+
+  return 0;
   
 }
   
-  void program(struct symbol* symbolList){
-    block(symbolList);
-    
-    if(symbolList.kind != periodsym)
-      printf("Period expected.\n");
+void program(){
+  block();  
+}
+
+void block(){
+  while(currentToken != nulsym){
+    printf("%d\n", currentToken);
+    if(currentToken == identsym)
+      printf("%s\n", tokenVal.string);
+    if(currentToken == numbersym)
+      printf("%d\n", tokenVal.numeric);
+    readToken();
   }
+}
+
+void statement(){
+
+}
+
+void condition(){
+
+}
   
-  void block(struct symbol* symbolList){
-    if(symbolList.kind == constsym){
-      while(symbolList.kind != commasym){
-        symbolList = symbolList.next;
-        
-        if(symbolList.kind != identsym)
-          printf("const must be followed by identifier.\n");
-      
-        symbolList = symbolList.next;
-        
-        if(symbolList.kind != eqsym){
-          if(symbolList.kind == becomessym)
-            printf("Use = instead of :=.\n");
-          else
-            printf("Identifier must be followed by =.\n");
-        }
-        
-        symbolList = symbolList.next;
-      
-        if(symbolList.kind != numbersym)
-          printf("= must be followed by a number.\n");
-      
-        symbolList = symbolList.next;
-      }
-      
-      symbolList = symbolList.next;
-      
-      if(symbolList.kind != semicolonsym)
-        printf("Semicolon missing.\n");
-    }
-    
-    if(symbolList.kind == intsym){
-      while(symbolList.kind == commasym){
-        symbolList = symbolList.next;
-        
-        if(symbolList.kind != identsym)
-          printf("var must be followed by identifier\n");
-          
-        symbolList = symbolList.next;
-      }
-      
-      if(symbolList.kind != semicolonsym)
-        printf("Error, from BLOCK, not semicolonsym\n");
-        
-      symbolList = symbolList.next;
-    }
-    
-    while(symbolList.kind == procsym){
-      symbolList = symbolList.next;
-      
-      if(symbolList.kind != identsym)
-        printf("procedure must be followed by identifier.\n");
-      
-      symbolList = symbolList.next;
-      
-      if(symbolList.kind != semicolonsym)
-        printf("Error, from BLOCK, not semicolonsym\n");
-        
-      symbolList = symbolList.next;
-      
-      block(symbolList);
-      
-      if(symbolList.kind != semicolonsym)
-        printf("Error, from BLOCK, not semicolonsym\n");
-      
-      symbolList = symbolList.next;
-    }
-    
-    statement(symbolList);
-    
+void expression(){
+
+}
+  
+void term(){
+
+  factor();
+
+}
+  
+void factor(){
+
+}
+
+void readToken(){
+  
+  //Making sure we still have input to read
+  if(feof(fin)){
+    currentToken = nulsym;
     return;
   }
-  
-  void statement(struct symbol* symbolList){
-    if(symbolList.kind == identsym){
-      symbolList = symbolList.next;
-      
-      if(symbolList.kind != becomessym)
-        printf("Error, from STATEMENT, not becomessym\n");
-        
-      symbolList = symbolList.next;
-      
-      expression(symbolList);
-    }
-    else if(symbolList.kind == syawsym){ // 'callsym'
-      symbolList = symbolList.next;
-      
-      if(symbolList.kind != identsym)
-        printf("syaw must be followed by an identifier.\n");
-      
-      symbolList = symbolList.next;
-    }
-    else if(symbolList.kind == sngaisym){ // 'beginsym'
-      symbolList = symbolList.next;
-      
-      statement(symbolList);
-      
-      while(symbolList.kind == semicolonsym){
-        symbolList = symbolList.next;
-        
-        statement(symbolList);
-      }
-      
-      if(symbolList.kind != fpesym) // 'endsym'
-        printf("Incorrect symbol following statement\n");
-      
-      symbolList = symbolList.next;
-    }
-    else if(symbolList.kind == ifsym){
-      symbolList = symbolList.next;
-      
-      condition(symbolList);
-      
-      if(symbolList.kind != tsakrrsym) // 'thensym'
-        printf("tsakrr expected.\n");
-      
-      symbolList = symbolList.next;
-      
-      statement(symbolList);
-    }
-    else if(symbolList.kind == tengkrrsym){ // 'whilesym'
-      symbolList = symbolList.next;
-      
-      condition(symbolList);
-      
-      if(symbolList.kind != sisym) // 'dosym'
-        printf("si expected.\n");
-      
-      symbolList = symbolList.next;
-      
-      statement(symbolList);
-    }
-    
-    return;
+
+  //Reading the new token in
+  fscanf(fin, "%d ", (int*)(&currentToken));
+
+  //Checking to see if we need to read anything else in
+  if(currentToken == identsym){
+    fscanf(fin, "%s ", tokenVal.string);
   }
-  
-  void condition(struct symbol* symbolList){
-    if(symbolList.kind == oddsym){
-      symbolList = symbolList.next;
-      
-      expression(symbolList);
-    }
-    else
-    {
-      expression(symbolList);
-      
-      if(symbolList.kind != RELATION)
-        printf("Relational operator expected\n");
-      
-      symbolList = symbolList.next;
-      
-      expression(symbolList);
-    }
-    
-    return;
+
+  if(currentToken == numbersym){
+    fscanf(fin, "%d ", &tokenVal.numeric);
   }
-  
-  void expression(struct symbol* symbolList){
-    if(symbolList.kind == plussym || symbolList.kind == minussym)
-      symbolList = symbolList.next;
-      
-    term(symbolList);
-    
-    while(symbolList == plussym || symbolList == slashsym){
-      symbolList = symbolList.next;
-      
-      term(symbolList);
-    }
-    
-    return;
-  }
-  
-  void term(struct symbol* symbolList){
-    factor(symbolList);
-    
-    while(symbolList.kind == multsym || symbolList.kind == slashsym){
-      symbolList = symbolList.next;
-      
-      factor(symbolList);
-    }
-    
-    return;
-  }
-  
-  void factor(struct symbol* symbolList){
-    if(symbolList.kind == identsym)
-      symbolList = symbolList.kind;
-    else if(symbolList.kind == numbersym)
-      symbolList = symbolList.kind;
-    else if(symbolList.kind == lparensym){
-      symbolList = symbolList.next;
-      
-      expression(symbolList);
-      
-      if(symbolList != rparensym)
-        printf("Right parenthesis missing.\n");
-    }
-    else
-      printf("Error, from FACTOR, generic error\n");
-    
-    return;
-  }
+
+}
